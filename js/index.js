3b3d6cba89d4f670201efb53cbdd176a25866c7c
rupikonna <rupikonna@byui.edu>
2020-05-04 22:37:10 -0600

    update for lesson 2

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
+let copyrightyear = new Date().getFullYear();
+    document.getElementById('year').innerHTML = copyrightyear;
+
 let modified = document.lastModified;
-document.getElementById("output").innerHTML = modified;
+    document.getElementById('lastModified').innerHTML = modified;
