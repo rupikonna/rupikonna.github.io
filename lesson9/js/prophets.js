fc0092b00a40c69a88ce521d3b89e468837675f7
rupikonna <rupikonna@byui.edu>
2020-06-17 23:14:55 -0600

    updates

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,23 +1,26 @@
-const requestURL = 'https:byui-cit230.github.io/lessons/lessonn-09/data/latter-day-prophets.json';
+const url = "https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json";
+const resultsElement = document.getElementById('results');
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let h = ""; // string to hold our HTML
 
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
-        for (let i = 0; i < prophets.length; i++) {}
+        let prophets = data.prophets; // The array of prophets
+
+        // Loop through each prophet of the prophets array
+        for (prophet of prophets) {
+            h += `<section>
+                 <h2>${prophet.name + ' ' + prophet.lastname}</h2>
+                 <p>Date of Birth: ${prophet.birthdate}</p>
+                 <p>Place of Birth: ${prophet.birthplace}</p>
+                 <img src="${prophet.imageurl}" alt="${prophet.name + prophet.lastname + '-' + prophet.order}">
+                </section>`;
+        }
+
+        // Close the div
+        h += `</div>`;
+
+        // Add the string we created to the results div
+        resultsElement.innerHTML = h;
     });
-
-const prophets = jsonObject['prophets'];
-
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
-
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
-
-image.setAttribute('src', prophets[i].imageurl);
